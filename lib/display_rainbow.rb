f40bdea3a15acf5f93c10b4f810f41e795634782
Adriena Williams <adrienawill@gmail.com>
2019-03-11 16:54:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,15 @@
 # Write your #display_rainbow method here
+
+def 
+  Print_business_card(person
+  )
+puts "name: #
+  {person[0]}, Age: #
+  {person[1]}, Occupation:#
+  {person[2]}"
+  puts "Contact: #
+  {person[3]}"
+end
+
+bob  = ["Bob", 46, "Juggler", "555-555-5555"]
+stefani = ["Stefani", 49, "Friefighter", "555-111-1111"]
